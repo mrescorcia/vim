2e53c251b049466d1623a9bb5d2d552bcd8d618e
mrescorcia <compumarana@gmail.com>
2021-06-23 18:53:54 -0500

    Nuevos archivos

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// funcion de suma total
+const { ayuda } = require('./helper.js')
+
 const suma = (ns) => {
   let acumulado = 0;
-  for (i = 0; i < ns.length; i++){
- 
+  for (i = 0; i < ns.length; i++) {
     acumulado += ns[i];
   }
 
@@ -10,7 +10,7 @@ const suma = (ns) => {
 } 
 
 const numeros = [1, 2, 3, 4, 5];
-// multiplicar * 2
+
 const multiplicados = numeros.map(x => x * 2)
 // a pares
 const parejas = numeros.map(x => [x, x])
